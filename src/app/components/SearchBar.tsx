5fc1eeddb127e7cba3946187bc371051161e037f
Tony <tonykurosaki117@gmail.com>
2024-03-08 19:05:56 -0600

    feat: Search functionality added

--- src/app/components/SearchBar.tsx
+++ src/app/components/SearchBar.tsx
@@ -1,18 +1,43 @@
 'use client'
 
+import { ChangeEvent, Dispatch, FormEvent, SetStateAction, useEffect, useState } from "react"
 import { FaSearch } from "react-icons/fa"
 
-export const SearchBar = () => {
+type PropTypes = {
+  setFilters : Dispatch<SetStateAction<string>>
+}
+
+export const SearchBar = ({ setFilters }: PropTypes) => {
+  const [search, setSearch] = useState('')
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value)
+  } 
+
+  useEffect(() => {
+    if(!search) setFilters(search)
+  },[search])
+
+  const handleSearch = (e: FormEvent<HTMLButtonElement>) => {
+    e.preventDefault()
+    setFilters(search)
+  }
+
   return <>
-    <div className="w-3/4 max-w-[700px] flex gap-2 p-1 pr-3 bg-gray-900 rounded-2xl items-center justify-center" >
+    <form className="w-3/4 max-w-[700px] flex gap-2 p-1 pr-3 bg-gray-900 rounded-2xl items-center justify-center">
       <input 
         type="text" 
         placeholder="Buscar..." 
+        onChange={handleChange}
         className=" w-full bg-black rounded-2xl p-2 focus:outline-none"
       />
-      <span className="cursor-pointer">
-        <FaSearch onClick={() => console.log("Search")}/>
-      </span>
-    </div>
+      <button
+        className="cursor-pointer"
+        type="submit"
+        onClick={handleSearch}
+      >
+        <FaSearch/>
+      </button>
+    </form>
   </>
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,14 +5,16 @@ import { SearchBar } from "./components/SearchBar";
 import { IconButton } from "./components/IconButton";
 import { useBlogPost } from "./hooks/useBlogPost";
 import { IBlogPost } from "./interfaces/blogPostInterface";
+import { useState } from "react";
 
 export default function Home() {
-  const { data, isLoading } = useBlogPost('')
+  const [filters, setFilters] = useState('')
+  const { data, isLoading } = useBlogPost(filters);
 
   return (
     <main className="flex min-h-screen flex-col items-center gap-20 py-10">
       <div className="w-full flex justify-center items-center gap-4">
-        <SearchBar/>
+        <SearchBar setFilters={setFilters} />
         <IconButton/>
       </div>
       <div className="w-3/4 max-md:w-full flex justify-center flex-wrap gap-10">
